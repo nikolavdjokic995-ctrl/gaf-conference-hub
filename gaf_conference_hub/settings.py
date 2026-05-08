d97ee2ed87e654d1f321583bf75d3a8abd121d25
Nikola <nikolavdjokic@gmail.com>
2026-05-08 09:14:18 +0200

    Enable debug

--- gaf_conference_hub/settings.py
+++ gaf_conference_hub/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-change-this-later"
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
