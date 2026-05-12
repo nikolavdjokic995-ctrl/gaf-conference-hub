3c309e6bfcad0ede348414db673a7f337022e64b
Nikola <nikolavdjokic@gmail.com>
2026-05-12 17:51:24 +0200

    Widen judge dashboard table

--- gaf_conference_hub/settings.py
+++ gaf_conference_hub/settings.py
@@ -145,3 +145,9 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+    "OVERWRITE": True,
+}
