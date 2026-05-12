a61538a30b29e5cde0f0f9de95f3ff534006665e
Nikola <nikolavdjokic@gmail.com>
2026-05-12 16:07:58 +0200

    Temporarily enable debug for error check

--- gaf_conference_hub/settings.py
+++ gaf_conference_hub/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-change-this-later"
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "gaf-conference-hub.onrender.com",
