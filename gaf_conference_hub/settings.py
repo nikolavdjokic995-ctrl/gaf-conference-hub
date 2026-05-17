1a0f845f9c34ad0c217ffd41530d01d965b92982
Nikola <nikolavdjokic@gmail.com>
2026-05-17 12:47:07 +0200

    Add SMTP timeout settings

--- gaf_conference_hub/settings.py
+++ gaf_conference_hub/settings.py
@@ -178,3 +178,12 @@ DEFAULT_FROM_EMAIL = os.getenv(
     EMAIL_HOST_USER or "Green Building Conference 2026 <no-reply@example.com>",
 )
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "False") == "True"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+EMAIL_TIMEOUT = int(os.environ.get("EMAIL_TIMEOUT", 10))
