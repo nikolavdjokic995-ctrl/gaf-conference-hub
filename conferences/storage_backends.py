045154f76b44f0d5a33e71986bbf49945d77c7ad
Nikola <nikolavdjokic@gmail.com>
2026-05-19 12:36:39 +0200

    Fix R2 path style endpoint

--- conferences/storage_backends.py
+++ conferences/storage_backends.py
@@ -5,14 +5,10 @@ from botocore.client import Config
 
 class R2Storage(S3Boto3Storage):
     bucket_name = os.getenv("R2_BUCKET_NAME")
-
-    endpoint_url = (
-        f"https://{os.getenv('R2_ACCOUNT_ID')}.r2.cloudflarestorage.com"
-    )
+    endpoint_url = f"https://{os.getenv('R2_ACCOUNT_ID')}.r2.cloudflarestorage.com"
 
     access_key = os.getenv("R2_ACCESS_KEY_ID")
     secret_key = os.getenv("R2_SECRET_ACCESS_KEY")
-
     region_name = "auto"
 
     file_overwrite = False
@@ -20,18 +16,9 @@ class R2Storage(S3Boto3Storage):
     querystring_auth = False
 
     config = Config(
-    signature_version="s3v4",
-    s3={
-        "addressing_style": "virtual"
-    },
-    retries={
-        "max_attempts": 3,
-        "mode": "standard"
-    }
-)
-
-use_ssl = True
-verify = False
+        signature_version="s3v4",
+        s3={"addressing_style": "path"},
+    )
 
 
 class HybridDocumentStorage(R2Storage):
