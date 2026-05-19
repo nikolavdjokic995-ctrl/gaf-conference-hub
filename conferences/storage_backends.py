ef4660f840e722250315f08eeb64c45295982b76
Nikola <nikolavdjokic@gmail.com>
2026-05-19 10:17:58 +0200

    Force R2 path style S3 config

--- conferences/storage_backends.py
+++ conferences/storage_backends.py
@@ -1,20 +1,21 @@
 import os
-
+from botocore.config import Config
 from storages.backends.s3 import S3Storage
 
 
 class HybridDocumentStorage(S3Storage):
+    bucket_name = os.environ.get("R2_BUCKET_NAME", "gaf-conference-papers")
     access_key = os.environ.get("R2_ACCESS_KEY_ID")
     secret_key = os.environ.get("R2_SECRET_ACCESS_KEY")
-    bucket_name = os.environ.get("R2_BUCKET_NAME")
-
     endpoint_url = os.environ.get("R2_ENDPOINT_URL")
-
     region_name = "auto"
 
     default_acl = None
     file_overwrite = False
+    querystring_auth = True
+    custom_domain = False
 
-    querystring_auth = False
-
-    addressing_style = "path"
+    config = Config(
+        signature_version="s3v4",
+        s3={"addressing_style": "path"},
+    )
