2862427626262c2eb776624d812fa3f88e3238d9
Nikola <nikolavdjokic@gmail.com>
2026-05-25 14:04:13 +0200

    Remove broken robots_txt import

--- gaf_conference_hub/urls.py
+++ gaf_conference_hub/urls.py
@@ -56,14 +56,12 @@ from conferences.views import (
     reviewer_topics,
     email_health_dashboard,
     run_email_automation_now,
-    robots_txt,
     sitemap_xml,
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
-    path("robots.txt", robots_txt, name="robots_txt"),
     path("sitemap.xml", sitemap_xml, name="sitemap_xml"),
     path(
         "google455867b1428963df.html",
